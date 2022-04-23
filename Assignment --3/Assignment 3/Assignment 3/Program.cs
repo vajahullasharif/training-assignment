27d948083585c8d923350324c28f0113252ef4fe
vajahullasharif <vajahullamohmmed@gmail.com>
2022-04-23 12:40:18 +0530

    c#Assignment-3

--- Assignment --3/Assignment 3/Assignment 3/Program.cs
+++ Assignment --3/Assignment 3/Assignment 3/Program.cs
@@ -11,13 +11,13 @@ namespace Assignment_3
         static void Main(string[] args)
         {
             // student marks program
-            Student_data student = new Student_data(1234, "abcd", "Btech", "1stsem", "cse");
+            Student_data student = new Student_data(1234, "sharif", "Btech", "1stsem", "mech");
             student.displaydata();
             student.displayresult();
 
             // car program
 
-            cars car = new cars(9999, "BMW", "SUV");
+            cars car = new cars(0007, "rollsroyce", "boattail");
             car.displaycardetails();
             cars car2 = new cars(500000);
 
